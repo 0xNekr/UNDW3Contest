623e363d2030453f3a304e53663e5be8343abb54
"0xNekr" <"maximecroteau.crypto@gmail.com">
2022-06-10 20:06:53 +0200

    fix

--- contracts/UNDW3isERC721.sol
+++ contracts/UNDW3isERC721.sol
@@ -3,18 +3,20 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
- * @title UNDW3isERC721
+ * @title UNDW3isERC1155
  * @dev Create by Discord Nekr#5112 for UNDW3 Lacoste art-contest because code is art !
  */
-
 contract UNDW3isERC1155 is ERC1155, Ownable {
 
     using Strings for uint;
 
     uint public price = 0.08 ether;
+    uint public currentSupply = 0;
     uint private constant max_supply = 11212;
+
 
     uint256 public constant Crocodile = 1;
 
@@ -24,8 +26,9 @@ contract UNDW3isERC1155 is ERC1155, Ownable {
 
     function mint() external payable {
         require(msg.value >= price);
-        require(totalSupply() < max_supply);
+        require(currentSupply < max_supply);
         require(onlyOneNFTPerWallet[msg.sender] == 0);
+        currentSupply += 1;
         onlyOneNFTPerWallet[msg.sender] += 1;
         _mint(msg.sender, Crocodile, 1, "");
     }
@@ -39,4 +42,12 @@ contract UNDW3isERC1155 is ERC1155, Ownable {
         );
     }
 
+    function reward() public pure returns (string memory) {
+        return "Lacoste List for Nekr#5112 / @0xNekr";
+    }
+
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
 }
