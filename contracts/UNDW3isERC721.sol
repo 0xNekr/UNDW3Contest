b34ff169e1f07f578812ffaa0aa82c0bbfcbdc86
"0xNekr" <"maximecroteau.crypto@gmail.com">
2022-06-10 18:53:30 +0200

    initial commit

---
+++ contracts/UNDW3isERC721.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+/**
+ * @title UNDW3isERC721
+ * @dev Create by Discord Nekr#5112 for UNDW3 Lacoste art-contest because code is art ! 
+ */
+contract UNDW3isERC721 is ERC721 {
+
+    constructor() ERC721("Lacoste UNDW3 by @0xNerk", "UNDW3") {}
+
+
+}
